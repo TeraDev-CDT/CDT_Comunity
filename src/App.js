b59180a7178608bb1aba503a402fbd0862da0440
TeraDev-CDT <80227611+TeraDev-CDT@users.noreply.github.com>
2021-09-13 12:57:17 -0400

    add router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBar from './component/NavBar';
 import Trusted from './component/Trusted';
 import Banner from './component/Banner';
 import Footer from './component/Footer';
+import { BrowserRouter as Router , Route, Switch, Redirect } from 'react-router-dom';
+import React from 'react';
+import Login from './Pages/User/Login';
+import Home from './Pages/Home/Home'
+import NewNavBar from './component/NewNavBar';
+import Forum from './Pages/Forum/Forum';
+
 
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
-      <Banner/>
-      <Trusted/>
-      <Trusted/>
-      <Trusted/>
-      <Footer/>
-
-    </div>
+  <div className="App">
+    
+    <div ClassName ="Container>">
+    
+    
+    
+    <Router>
+    <NewNavBar/>
+    
+      <main>
+        <Switch>
+        <Route path= '/' exact component ={Home}/>
+        <Route path='/Home' component = {Home} />
+        <Route path="/Login" component ={Login} />
+        <Route path="/Forum" component ={Forum} />
+          
+        </Switch>
+        </main>
+      </Router>
+      
+      
+     
+    
+     </div>
+     </div>
+      
+      
   )
 }
 
